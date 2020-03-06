cfc27d3f39028052929f091a53e74ae1b18e8431
Alice <alicenjoroge707@gmail.com>
2020-03-06 07:57:58 +0300

    nesting components

---
+++ myapp/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import New from './New';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Welcome</p>
+        <New />
+      </div>
+
+    );
+  }
+
+}
+
+export default App;
---
+++ myapp/src/New.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class New extends Component {
+    render() {
+        return (
+            <div>Name : Alice </div>
+
+        )
+    }
+}
+
+export default New;
